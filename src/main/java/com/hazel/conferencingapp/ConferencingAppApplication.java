80a510e0f97cf650442def2f4da58380658ffcce
Hazel <hazelchikara@hotmail.com>
2023-05-03 15:43:49 +0200

    initial commit

---
+++ src/main/java/com/hazel/conferencingapp/ConferencingAppApplication.java
@@ -0,0 +1,13 @@
+package com.hazel.conferencingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferencingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferencingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hazel/conferencingapp/ConferencingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hazel.conferencingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferencingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
